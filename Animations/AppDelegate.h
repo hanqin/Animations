183e2e7a3c7fd05e8cf8c1c75c97ce39c962fa09
Han Qin <hanhaify@gmail.com>
2015-09-08 12:47:51 +0800

    Initial Commit

---
+++ Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animations
+//
+//  Created by hqin on 15/9/8.
+//  Copyright (c) 2015年 hqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animations/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Animations
+//
+//  Created by hqin on 15/9/8.
+//  Copyright (c) 2015年 hqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
