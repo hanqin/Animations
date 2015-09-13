ae3c8be5842d0feb4514269d04830f155d539248
qinhan <qinhan@xingshulin.com>
2015-09-13 21:53:05 +0800

    animation group and transform.rotation

--- Animations/explicit/BezierPathAnimViewController.m
+++ Animations/explicit/BezierPathAnimViewController.m
@@ -18,11 +18,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    CGPoint startingPoint = CGPointMake(0, 150);
+    CGPoint startingPoint = CGPointMake(150, 150);
     
     UIBezierPath *bezierPath = [UIBezierPath new];
     [bezierPath moveToPoint:startingPoint];
-    [bezierPath addCurveToPoint:CGPointMake(300, 150) controlPoint1:CGPointMake(75, 0) controlPoint2:CGPointMake(225, 300)];
+    [bezierPath addCurveToPoint:CGPointMake(450, 450) controlPoint1:CGPointMake(75, 250) controlPoint2:CGPointMake(225, 350)];
     
     CAShapeLayer *pathLayer = [CAShapeLayer layer];
     pathLayer.path = bezierPath.CGPath;
@@ -38,14 +38,27 @@
     shipLayer.contents = (__bridge id)[UIImage imageNamed:@"space_ship"].CGImage;
     [self.container.layer addSublayer:shipLayer];
     
-    CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
-    animation.keyPath = @"position";
-    animation.duration = 4.0;
-    animation.path = bezierPath.CGPath;
-    animation.rotationMode = kCAAnimationRotateAuto;
-    animation.repeatCount = 50;
-    [shipLayer addAnimation:animation forKey:nil];
+    CAKeyframeAnimation *flyAnimation = [CAKeyframeAnimation animation];
+    flyAnimation.keyPath = @"position";
+    flyAnimation.duration = 4.0;
+    flyAnimation.path = bezierPath.CGPath;
+    flyAnimation.rotationMode = kCAAnimationRotateAuto;
+    flyAnimation.repeatCount = 50;
+    
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animation];
+    rotationAnimation.keyPath = @"transform.rotation";
+    rotationAnimation.duration = 2.0;
+    rotationAnimation.byValue = @(M_PI * 2);
+    rotationAnimation.repeatCount = 50;
+    
+    CAAnimationGroup *animationGroup = [CAAnimationGroup animation];
+    animationGroup.animations = @[flyAnimation, rotationAnimation];
+    animationGroup.duration = 4.0;
+    animationGroup.repeatCount = 50;
+    
+    [shipLayer addAnimation:animationGroup forKey:nil];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
